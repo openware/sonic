7e1dcef2e71997c6e2f73beb15c9024ec8b508b9
Danylo Patsora <dpatsora@heliostech.fr>
2021-01-14 02:19:00 +0200

    Feature: Update Sonic Config

--- config.go
+++ config.go
@@ -1,16 +1,12 @@
 package sonic
 
+import (
+	"github.com/openware/pkg/database"
+)
+
 // Config is the application configuration structure
 type Config struct {
-	// TODO move to database package
-	Database struct {
-		Driver string `yaml:"driver" env:"DATABASE_DRIVER" env-description:"Database driver"`
-		Host   string `yaml:"host" env:"DATABASE_HOST" env-description:"Database host"`
-		Port   string `yaml:"port" env:"DATABASE_PORT" env-description:"Database port"`
-		Name   string `yaml:"name" env:"DATABASE_NAME" env-description:"Database name"`
-		User   string `yaml:"user" env:"DATABASE_USER" env-description:"Database user"`
-		Pass   string `env:"DATABASE_PASS" env-description:"Database user password"`
-	} `yaml:"database"`
+	Database database.Config `yaml:"database"`
 	// TODO Create a redis and vault package
 	Redis struct {
 		Host string `yaml:"host" env:"REDIS_HOST" env-description:"Redis Server host" env-default:"localhost"`

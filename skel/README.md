937f3980ead7264881b504e05ef8754cb12e1550
Andrew Kushniruk <akushniruk@heliostech.fr>
2021-02-08 17:34:55 +0200

    Update: build asset pipeline (#6)
    * Add walk match + update index.html

--- skel/README.md
+++ skel/README.md
@@ -1,3 +1,22 @@
 # Go-Gin web application
 
-This application was generated with [generator-gin](https://github.com/openware/generator-gin)
+This application was generated with [sonic](https://github.com/openware/sonic)
+
+# How to run sonic with client
+
+1. Put your frontend application to the client folder
+
+2. Run makefile
+```
+make asset
+```
+It will run build in the client folder and then all build files will be moved to public/assets.
+#### **Be sure that you build your client (frontend) to the build folder, if it's another folder you can update your client (frontend) or makefile**
+
+3. Run go server 
+```
+go run app.go serve
+```
+
+# Troubleshooting
+**If it doesn't work and you see the white screen, check the order of the import files in index.html**
--- skel/handlers/index.go
+++ skel/handlers/index.go
@@ -1,7 +1,11 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/foolin/goview/supports/ginview"
 	"github.com/gin-gonic/gin"
@@ -31,8 +35,21 @@ func Setup(app *sonic.Runtime) {
 
 // index render with master layer
 func index(ctx *gin.Context) {
+	cssFiles, err := FilesPaths("/public/assets/*.css")
+	if err != nil {
+		log.Println("filePaths:", "Can't take list of paths for css files: "+err.Error())
+	}
+
+	jsFiles, err := FilesPaths("/public/assets/*.js")
+	if err != nil {
+		log.Println("filePaths", "Can't take list of paths for js files in public folder: "+err.Error())
+	}
+
 	ctx.HTML(http.StatusOK, "index", gin.H{
-		"title": "Index title!",
+		"title":    "Index title!",
+		"cssFiles": cssFiles,
+		"jsFiles":  jsFiles,
+		"rootID":   "root",
 		"add": func(a int, b int) int {
 			return a + b
 		},
@@ -48,3 +65,25 @@ func emptyPage(ctx *gin.Context) {
 func version(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"Version": Version})
 }
+
+func FilesPaths(pattern string) ([]string, error) {
+	var matches []string
+
+	fullPath, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	matches, err = filepath.Glob(fullPath + pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, _ := range matches {
+		matches[i] = strings.Replace(matches[i], fullPath, "", -1)
+	}
+
+	return matches, nil
+}
+
+// TODO: Add a version handler which return the value of main.Version

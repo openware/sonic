78376f136a77039c08004d8dc488cd6fc5442418
Tee <tee@yelllow.com>
2021-02-11 11:37:29 +0700

    Fix: code format and simplify handle_err

--- scripts/svm.sh
+++ scripts/svm.sh
@@ -9,49 +9,43 @@ svm() {
   local i
 
   #  FIXME: loop is uselless?
-  for i in "$@"
-  do
-    case $i in 
-      '-h'|'help'|'--help')
-        echo 'Usage:'
-        echo '  svm --help                                  Show this message'
-        echo '  svm create <project name>                   Install sonic'
-        return
-        ;;
-      *)
+  for i in "$@"; do
+    case $i in
+    '-h' | 'help' | '--help')
+      echo 'Usage:'
+      echo '  svm --help                                  Show this message'
+      echo '  svm create <project name>                   Install sonic'
+      return
+      ;;
+    *) ;;
     esac
   done
 
   local COMMAND
   COMMAND="${1}"
 
-  case $COMMAND in 
-    'create' | 'i')
-      create "${2}"
-      ;;
-    *)
-      hander_err "Command ${COMMAND} is not found"
-      ;;
+  case $COMMAND in
+  'create' | 'i')
+    create "${2}"
+    ;;
+  *)
+    hander_err "Command ${COMMAND} is not found"
+    ;;
   esac
 }
 
-hander_err ()
-{
-  local ERROR_MSG
-  ERROR_MSG="${1}"
-  # FIXME: echo "=> Error: ${1}" ?
-  echo "=> Error: ${ERROR_MSG}" 
+hander_err() {
+  echo "=> Error: ${1}"
 }
 
-create ()
-{
+create() {
   local GITPATH
   GITPATH="$1"
   local DIR
   local ADDR
 
   IFS='/'
-  read -A ADDR <<< "$GITPATH"
+  read -A ADDR <<<"$GITPATH"
   DIR=${ADDR[${#ADDR[@]}]}
 
   if [ -d ${DIR} ]; then
@@ -61,7 +55,7 @@ create ()
 
     cp -r $HOME/.svm/skel ${DIR}
     sed -i "" "s|github.com/openware/sonic/skel|${GITPATH}|g" ${DIR}/**/*.go ${DIR}/go.mod
-    
+
     git init -q ${DIR}
     cd ${DIR}
     git add .
